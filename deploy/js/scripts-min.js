bb9b1def58c8634d187d9b608baab9715b87a08c
Terynrose <63327501+Terynrose@users.noreply.github.com>
2020-10-06 17:51:34 -0400

    section 1 push

--- deploy/js/scripts-min.js
+++ deploy/js/scripts-min.js
@@ -1,1 +1,1 @@
-import{section1Animation}from".section-1.js";section1Animation();
+import{gsap}from"gsap";console.log(gsap);import{section1Animation}from".section-1.js";section1Animation();
--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,3 +1,6 @@
+import {gsap} from "gsap";
+
+console.log(gsap);
 
 
 import {section1Animation} from ".section-1.js";
